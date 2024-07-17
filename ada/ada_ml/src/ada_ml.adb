33d67952214b8ceccca93c3d8941121fdea85ee4
saulius <saulius@cbc6df6a-4905-47e4-957a-ae58ab66f2a2>
2024-07-17 09:05:40 +0000

    Loading the image from the external file and processing it. The result is the same as in the original demo :).
    ada/ (saulius@starta)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-ml/trunk@13 cbc6df6a-4905-47e4-957a-ae58ab66f2a2

--- ada/ada_ml/src/ada_ml.adb
+++ ada/ada_ml/src/ada_ml.adb
@@ -1,10 +1,12 @@
+pragma Ada_2022;
+
 with Ada.Text_IO;      use Ada.Text_IO;
 with Ada.Command_Line; use Ada.Command_Line;
 with Ada.Exceptions;   use Ada.Exceptions;
 
 with ONNX_Runtime.Environments;
 with ONNX_Runtime.Sessions;
-with ONNX_Runtime.Values;
+with ONNX_Runtime.Values; use ONNX_Runtime.Values;
 
 with PNM_Reader;
 
@@ -20,44 +22,6 @@ procedure Ada_Ml is
    
    -- -------------------------------------------------------------------------
    
-   o : constant Float := 0.0;
-   X : constant Float := 1.0;
-
-   s28x28 : constant := 28 * 28;
-
-   pragma Style_Checks (Off);
-   Image : constant ONNX_Runtime.Values.Float_Array (1 .. s28x28) :=
-     (o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  1
-      o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  2
-      o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  3
-      o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  4
-      o,o,o,o,o,o,X,X,X,X,X,X,X,X,X,X,o,o,o,o,o,o,o,o,o,o,o,o,   --  5
-      o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,   --  6
-      o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,   --  7
-      o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,   --  8
-      o,o,o,o,o,o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  9
-      o,o,o,o,o,o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  10
-      o,o,o,o,o,o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  11
-      o,o,o,o,o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  12
-      o,o,o,o,o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  13
-      o,o,o,o,o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  14
-      o,o,o,o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  15
-      o,o,o,o,o,o,o,o,o,X,X,X,X,X,X,X,o,o,o,o,o,o,o,o,o,o,o,o,   --  16
-      o,o,o,o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  17
-      o,o,o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  18
-      o,o,o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  19
-      o,o,o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  20
-      o,o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  21
-      o,o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  22
-      o,o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  23
-      o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  24
-      o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  25
-      o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  26
-      o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  27
-      o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o);  --  28
-   
-   -- pragma Style_Checks (On);
-   
    Env : constant ONNX_Runtime.Environments.Environment :=
      ONNX_Runtime.Environments.Create_Environment;
 
@@ -71,8 +35,6 @@ procedure Ada_Ml is
    function PNM_Raster_To_Array (Image : PNM_Image_Type)
                                 return ONNX_Runtime.Values.Float_Array
    is
-      use ONNX_Runtime.Values;
-      
       Image_Size : constant Element_Index :=
         Element_Index(Image.Raster.M * Image.Raster.N);
       
@@ -120,7 +82,15 @@ begin
          
          declare
             Input : constant ONNX_Runtime.Values.Value_Array (1 .. 1) :=
-              (1 => ONNX_Runtime.Values.Create_Tensor (Image, (1, 1, 28, 28)));
+              (1 => ONNX_Runtime.Values.Create_Tensor
+                 (
+                  PNM_Raster_To_Array (PNM_Image),
+                  (1, 1,
+                   Element_Index (PNM_Image.Raster.N),
+                   Element_Index (PNM_Image.Raster.M)
+                  )
+                 )
+              );
          begin
             Session.Run (Input, Output);
             Output (1).Get_Data (Probability);
