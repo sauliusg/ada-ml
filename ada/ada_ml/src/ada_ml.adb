e3d3179dd31a7d1c8aa941bfaed987899517160b
saulius <saulius@cbc6df6a-4905-47e4-957a-ae58ab66f2a2>
2024-07-17 08:52:31 +0000

    Adding code to load a PNM image and rearranging code to run the ML model.
    ada/ (saulius@starta)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-ml/trunk@12 cbc6df6a-4905-47e4-957a-ae58ab66f2a2

--- ada/ada_ml/src/ada_ml.adb
+++ ada/ada_ml/src/ada_ml.adb
@@ -1,4 +1,6 @@
-with Ada.Text_IO;
+with Ada.Text_IO;      use Ada.Text_IO;
+with Ada.Command_Line; use Ada.Command_Line;
+with Ada.Exceptions;   use Ada.Exceptions;
 
 with ONNX_Runtime.Environments;
 with ONNX_Runtime.Sessions;
@@ -7,6 +9,17 @@ with ONNX_Runtime.Values;
 with PNM_Reader;
 
 procedure Ada_Ml is
+
+   pragma Style_Checks (Off);
+   
+   type PNM_16bin_Pixel is mod 2**16;
+   
+   package PNM_16bit_Reader is new PNM_Reader (PNM_16bin_Pixel);
+   
+   use PNM_16bit_Reader;
+   
+   -- -------------------------------------------------------------------------
+   
    o : constant Float := 0.0;
    X : constant Float := 1.0;
 
@@ -42,32 +55,92 @@ procedure Ada_Ml is
       o,o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  26
       o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,   --  27
       o,o,o,o,o,o,o,o,X,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o,o);  --  28
-   pragma Style_Checks (On);
+   
+   -- pragma Style_Checks (On);
    
    Env : constant ONNX_Runtime.Environments.Environment :=
      ONNX_Runtime.Environments.Create_Environment;
 
-   Session : ONNX_Runtime.Sessions.Session :=
-     Env.Create_Session (Model => "mnist.onnx");
-   --  https://github.com/microsoft/onnxruntime-inference-examples/
-   --  raw/main/c_cxx/MNIST/mnist.onnx
-
    Output : ONNX_Runtime.Values.Value_Array (1 .. 1);
-   Input  : constant ONNX_Runtime.Values.Value_Array (1 .. 1) :=
-     (1 => ONNX_Runtime.Values.Create_Tensor (Image, (1, 1, 28, 28)));
 
    Probability : ONNX_Runtime.Values.Float_Array (0 .. 9);
    Max         : ONNX_Runtime.Values.Element_Index := Probability'First;
    
+   -- -------------------------------------------------------------------------
+
+   function PNM_Raster_To_Array (Image : PNM_Image_Type)
+                                return ONNX_Runtime.Values.Float_Array
+   is
+      use ONNX_Runtime.Values;
+      
+      Image_Size : constant Element_Index :=
+        Element_Index(Image.Raster.M * Image.Raster.N);
+      
+      Retval : ONNX_Runtime.Values.Float_Array (1 .. Image_Size);
+      
+      K : Element_Index := 1;
+   begin
+      for I in Image.Raster.Pixels'Range (1) loop
+         for J in Image.Raster.Pixels'Range (2) loop
+            Retval (K) := Float (Image.Raster.Pixels (I, J));
+            K := K + 1;
+         end loop;
+      end loop;
+      return Retval;
+   end;
+   
+   -- -------------------------------------------------------------------------
+   
+   NO_MODEL_PROVIDED : exception;
+   
+   No_Model_File_Status : constant Ada.Command_Line.Exit_Status := 1;
+   
+   Model_File_Name : constant String :=
+     (if Argument_Count > 0 then Argument (1) else "");
+   
 begin
-   Session.Run (Input, Output);
-   Output (1).Get_Data (Probability);
+   
+   if Argument_Count = 0 then
+      raise NO_MODEL_PROVIDED with
+        "a model file name must be provided " &
+        "as the first command line argument";
+   end if;
+   
+   declare
+      Session : ONNX_Runtime.Sessions.Session :=
+        Env.Create_Session (Model => Model_File_Name);
+      --  https://github.com/microsoft/onnxruntime-inference-examples/
+      --  raw/main/c_cxx/MNIST/mnist.onnx
+      PNM_Image : PNM_Image_Type;
+   begin
 
-   for J in Probability'Range loop
-      if Probability (Max) < Probability (J) then
-         Max := J;
-      end if;
-   end loop;
+      for I in 2 .. Argument_Count loop
+         
+         Load_Raster (Argument (I), PNM_Image);
+         
+         declare
+            Input : constant ONNX_Runtime.Values.Value_Array (1 .. 1) :=
+              (1 => ONNX_Runtime.Values.Create_Tensor (Image, (1, 1, 28, 28)));
+         begin
+            Session.Run (Input, Output);
+            Output (1).Get_Data (Probability);
 
-   Ada.Text_IO.Put_Line ("Result:" & Max'Image);
+            for J in Probability'Range loop
+               if Probability (Max) < Probability (J) then
+                  Max := J;
+               end if;
+            end loop;
+
+            Ada.Text_IO.Put_Line ("Result:" & Max'Image);
+         end;
+
+      end loop;
+      
+   end;
+   
+exception
+   when Exception_Occurence : NO_MODEL_PROVIDED =>
+      Put_Line (Command_Name & ": " & Exception_Message(Exception_Occurence));
+      Ada.Command_Line.Set_Exit_Status (No_Model_File_Status);
+   
 end Ada_Ml;
