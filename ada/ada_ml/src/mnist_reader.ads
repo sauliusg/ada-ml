8aae5ab14e5377634e901e5f509cc14244403f27
saulius <saulius@cbc6df6a-4905-47e4-957a-ae58ab66f2a2>
2024-07-17 12:43:07 +0000

    Naming Big Endian words as explicit type.
    ada/ (saulius@starta)
    
    
    git-svn-id: svn+ssh://saulius.grazulis.lt/home/saulius/svn-repositories/ada-ml/trunk@22 cbc6df6a-4905-47e4-957a-ae58ab66f2a2

--- ada/ada_ml/src/mnist_reader.ads
+++ ada/ada_ml/src/mnist_reader.ads
@@ -8,13 +8,15 @@ package MNIST_Reader is
    type Word is mod 2**16;
    type DWord is mod 2**32;
    
+   type Big_Endian_DWord is mod 2**32;
+   
    type MNIST_Dataset_Header_Type is record
       Empty : Word;     -- should be filled with zeroes;
       Data_Type : Byte; -- 0x08: unsigned byte, ... (https://github.com/cvdfoundation/mnist)
       NDim : Byte;
    end record;
    
-   type MNIST_Dataset_Record_Type is new DWord;
+   type MNIST_Dataset_Record_Type is new Big_Endian_DWord;
 
    function N_Dimensions (Header : MNIST_Dataset_Header_Type) return Natural;
    
